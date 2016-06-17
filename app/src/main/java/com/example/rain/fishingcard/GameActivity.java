40960404554dbf3c5c5ac190fa1252c0d80f643f
rainf0rest <405763281@qq.com>
2016-06-17 23:55:10 +0800

    加入了开始游戏代码逻辑

---
+++ app/src/main/java/com/example/rain/fishingcard/GameActivity.java
@@ -0,0 +1,15 @@
+package com.example.rain.fishingcard;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by rain on 2016/6/17.
+ */
+public class GameActivity extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_play);
+    }
+}
--- app/src/main/java/com/example/rain/fishingcard/MainActivity.java
+++ app/src/main/java/com/example/rain/fishingcard/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.rain.fishingcard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    com.gc.materialdesign.views.ButtonRectangle startbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startbtn = (com.gc.materialdesign.views.ButtonRectangle) findViewById(R.id.startbtn);
+
+        startbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
