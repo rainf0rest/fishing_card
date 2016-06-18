69b58b680c5a5cb8f6d5ce3cd75a692ad54f1e70
rainf0rest <405763281@qq.com>
2016-06-19 01:33:35 +0800

    用TextView代替Button

--- app/src/main/java/com/example/rain/fishingcard/GameActivity.java
+++ app/src/main/java/com/example/rain/fishingcard/GameActivity.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
  */
 public class GameActivity extends Activity{
 
-    com.gc.materialdesign.views.ButtonRectangle fish1, fish2, fish3, fish4, fish5, rod1, rod2, rod3;
+    TextView fish1, fish2, fish3, fish4, fish5;
 
     Poker gamePoker = new Poker();
 
@@ -19,40 +19,30 @@ public class GameActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_play);
 
-        fish1 = (com.gc.materialdesign.views.ButtonRectangle) findViewById(R.id.fishbtn1);
-        fish2 = (com.gc.materialdesign.views.ButtonRectangle) findViewById(R.id.fishbtn2);
-        fish3 = (com.gc.materialdesign.views.ButtonRectangle) findViewById(R.id.fishbtn3);
-        fish4 = (com.gc.materialdesign.views.ButtonRectangle) findViewById(R.id.fishbtn4);
-        fish5 = (com.gc.materialdesign.views.ButtonRectangle) findViewById(R.id.fishbtn5);
-        rod1 = (com.gc.materialdesign.views.ButtonRectangle) findViewById(R.id.rodbtn1);
-        rod2 = (com.gc.materialdesign.views.ButtonRectangle) findViewById(R.id.rodbtn2);
-        rod3 = (com.gc.materialdesign.views.ButtonRectangle) findViewById(R.id.rodbtn3);
+        fish1 = (TextView) findViewById(R.id.fish1);
+        fish2 = (TextView) findViewById(R.id.fish2);
+        fish3 = (TextView) findViewById(R.id.fish3);
+        fish4 = (TextView) findViewById(R.id.fish4);
+        fish5 = (TextView) findViewById(R.id.fish5);
 
-        fish1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fish1.setText("start");
-            }
-        });
-
-        //init();
+        init();
 
     }
 
-    /*void init() {
+    void init() {
         fish1.setText("" + getAPoint());
         fish2.setText("" + getAPoint());
         fish3.setText("" + getAPoint());
         fish4.setText("" + getAPoint());
         fish5.setText("" + getAPoint());
-        rod1.setText("" + getAPoint());
+        /*rod1.setText("" + getAPoint());
         rod2.setText("" + getAPoint());
-        rod3.setText("" + getAPoint());
+        rod3.setText("" + getAPoint());*/
     }
 
     int getAPoint() {
         Card card = gamePoker.getACacd();
         return card.getPoint();
     }
-    */
+
 }
--- app/src/main/java/com/example/rain/fishingcard/MainActivity.java
+++ app/src/main/java/com/example/rain/fishingcard/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, GameActivity.class);
                 startActivity(intent);
+                //startbtn.setText("I like");
             }
         });
     }
