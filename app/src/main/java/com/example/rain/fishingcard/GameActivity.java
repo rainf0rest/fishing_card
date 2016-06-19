fd28970f5a301cceb18e54a7bc38e10ca5fdefdf
rainf0rest <405763281@qq.com>
2016-06-19 15:48:23 +0800

    实现游戏逻辑

--- app/src/main/java/com/example/rain/fishingcard/GameActivity.java
+++ app/src/main/java/com/example/rain/fishingcard/GameActivity.java
@@ -36,7 +36,72 @@ public class GameActivity extends Activity{
         cardLaveText = (TextView) findViewById(R.id.cardLave);
         comboText = (TextView) findViewById(R.id.combo);
 
+        fish1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                work(1, getNumber(fish1.getText().toString()));
+            }
+        });
+
+        fish2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                work(2, getNumber(fish2.getText().toString()));
+            }
+        });
+
+        fish3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                work(3, getNumber(fish3.getText().toString()));
+            }
+        });
+
+        fish4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                work(4, getNumber(fish4.getText().toString()));
+            }
+        });
+
+        fish5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                work(5, getNumber(fish5.getText().toString()));
+            }
+        });
+
+        rod1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                work(-1, getNumber(rod1.getText().toString()));
+            }
+        });
+
+        rod2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                work(-2, getNumber(rod2.getText().toString()));
+            }
+        });
+
+        rod3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                work(-3, getNumber(rod3.getText().toString()));
+            }
+        });
+
+        rod4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                work(-4, getNumber(rod4.getText().toString()));
+            }
+        });
+
         init();
+
+
 
     }
 
@@ -56,6 +121,7 @@ public class GameActivity extends Activity{
         scoreText.setText("score:" + score);
         cardLaveText.setText("" + gamePoker.getLave());
         comboText.setText("" + combo);
+
 
         /*rod1.setText("" + getAPoint());
         rod2.setText("" + getAPoint());
@@ -110,5 +176,73 @@ public class GameActivity extends Activity{
                 break;
         }
         return i;
+    }
+
+    int shoupai = 0, pupai = 0, shouchoose = 0, pu = 0;
+    int pupaich[] = {0,0,0,0,0,0,0};
+
+    void work(int type, int pp) {
+        if(type < 0){
+            shoupai = pp;
+            shouchoose = type;
+        }else {
+            pupai = pupai + pp;
+            pupaich[type] = 1;
+        }
+
+        if(shoupai == pupai && shoupai != 0) {
+            check();
+        }
+        comboText.setText("" + shouchoose);
+
+    }
+
+    void check() {
+        switch(shouchoose) {
+            case -1:
+                rod1.setText("" + getAPoint());
+                break;
+            case -2:
+                rod2.setText("" + getAPoint());
+                break;
+            case -3:
+                rod3.setText("" + getAPoint());
+                break;
+            case -4:
+                rod4.setText("" + getAPoint());
+                break;
+        }
+        for(int i = 1; i <= 5; i++) {
+            if(pupaich[i] == 1) {
+                switch(i){
+                    case 1:
+                        fish1.setText("" + getAPoint());
+                        break;
+                    case 2:
+                        fish2.setText("" + getAPoint());
+                        break;
+                    case 3:
+                        fish3.setText("" + getAPoint());
+                        break;
+                    case 4:
+                        fish4.setText("" + getAPoint());
+                        break;
+                    case 5:
+                        fish5.setText("" + getAPoint());
+                        break;
+                }
+            }
+        }
+        shouchoose = 0;
+        shoupai = 0;
+        pupai = 0;
+        for(int i = 0; i < pupaich.length; i++) {
+            pupaich[i] = 0;
+        }
+
+        scoreText.setText("score:" + score);
+        cardLaveText.setText("" + gamePoker.getLave());
+        //comboText.setText("" + shouchoose);
+        comboText.setText("check");
     }
 }
