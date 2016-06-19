37f793dffc5a428cda7cf9446c61e0f513ebf086
rainf0rest <405763281@qq.com>
2016-06-19 14:59:26 +0800

    poker point fiction

--- app/src/main/java/com/example/rain/fishingcard/GameActivity.java
+++ app/src/main/java/com/example/rain/fishingcard/GameActivity.java
@@ -10,8 +10,9 @@ import android.widget.TextView;
  */
 public class GameActivity extends Activity{
 
-    TextView fish1, fish2, fish3, fish4, fish5;
-
+    TextView fish1, fish2, fish3, fish4, fish5, fish6, fish7, fish8,
+            rod1, rod2, rod3, rod4, scoreText, cardLaveText, comboText;
+    int combo = 0, score = 0;
     Poker gamePoker = new Poker();
 
     @Override
@@ -24,6 +25,16 @@ public class GameActivity extends Activity{
         fish3 = (TextView) findViewById(R.id.fish3);
         fish4 = (TextView) findViewById(R.id.fish4);
         fish5 = (TextView) findViewById(R.id.fish5);
+        fish6 = (TextView) findViewById(R.id.fish6);
+        fish7 = (TextView) findViewById(R.id.fish7);
+        fish8 = (TextView) findViewById(R.id.fish8);
+        rod1 = (TextView) findViewById(R.id.rod1);
+        rod2 = (TextView) findViewById(R.id.rod2);
+        rod3 = (TextView) findViewById(R.id.rod3);
+        rod4 = (TextView) findViewById(R.id.rod4);
+        scoreText = (TextView) findViewById(R.id.score);
+        cardLaveText = (TextView) findViewById(R.id.cardLave);
+        comboText = (TextView) findViewById(R.id.combo);
 
         init();
 
@@ -35,14 +46,69 @@ public class GameActivity extends Activity{
         fish3.setText("" + getAPoint());
         fish4.setText("" + getAPoint());
         fish5.setText("" + getAPoint());
+        fish6.setVisibility(View.INVISIBLE);
+        fish7.setVisibility(View.INVISIBLE);
+        fish8.setVisibility(View.INVISIBLE);
+        rod1.setText("" + getAPoint());
+        rod2.setText("" + getAPoint());
+        rod3.setText("" + getAPoint());
+        rod4.setText("" + getAPoint());
+        scoreText.setText("score:" + score);
+        cardLaveText.setText("" + gamePoker.getLave());
+        comboText.setText("" + combo);
+
         /*rod1.setText("" + getAPoint());
         rod2.setText("" + getAPoint());
         rod3.setText("" + getAPoint());*/
     }
 
-    int getAPoint() {
+    String getAPoint() {
         Card card = gamePoker.getACacd();
-        return card.getPoint();
+        int point = card.getPoint();
+        String s = "A";
+        if(point >= 1 && point <= 9) {
+            s =  "" + point++;
+        }
+        else {
+            switch (point) {
+                case 0:
+                    s = "A";
+                    break;
+                case 10:
+                    s = "J";
+                    break;
+                case 11:
+                    s = "Q";
+                    break;
+                case 12:
+                    s = "K";
+                    break;
+                default:
+                    break;
+            }
+        }
+        return s;
     }
 
+    int getNumber(String s) {
+        int i;
+        switch(s) {
+            case "A":
+                i = 1;
+                break;
+            case "J":
+                i = 11;
+                break;
+            case "Q":
+                i = 12;
+                break;
+            case "K":
+                i = 13;
+                break;
+            default:
+                i = Integer.parseInt(s);
+                break;
+        }
+        return i;
+    }
 }
